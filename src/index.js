2e629465158dcd950ac534df2e7844b91196a444
Adrian <kontakt@adrianflak.pl>
2023-11-19 10:39:42 +0000

    small update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const app = require("./app")
 const port = process.env.PORT || 3000
 
 app.listen(port, () => 
-	console.log("Aplikacja nasłuchuje na porcie 3000"),
+	console.log("Aplikacja nasłuchuje na porcie 3000 !!!"),
 
 );
